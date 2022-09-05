7ef97b64f938732aa97479d94927e0028d11d467
suyantoo <bagusangue1@gmail.com>
2022-09-04 20:33:20 -0700

    menambahkan migration

---
+++ app/Http/Controllers/KadegoriController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kadegori;
+use Illuminate\Http\Request;
+
+class KadegoriController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kadegori  $kadegori
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kadegori $kadegori)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kadegori  $kadegori
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kadegori $kadegori)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kadegori  $kadegori
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kadegori $kadegori)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kadegori  $kadegori
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kadegori $kadegori)
+    {
+        //
+    }
+}
